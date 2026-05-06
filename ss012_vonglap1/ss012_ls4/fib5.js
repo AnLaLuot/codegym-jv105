73fd5d8771b4ec27ae414e12ca1f7a52e3b685a8
LianPro <boyintrouble.1310@gmail.com>
2026-05-06 14:52:23 +0700

    ss012_ls4

---
+++ ss012_vonglap1/ss012_ls4/fib5.js
@@ -0,0 +1,16 @@
+function find() {
+    let a = 0, b = 1;
+
+    while (true) {
+        let next = a + b;
+
+        if (next % 5 === 0) {
+            document.getElementById("result").innerText =
+                "Số đầu tiên chia hết cho 5 là: " + next;
+            break;
+        }
+
+        a = b;
+        b = next;
+    }
+}
