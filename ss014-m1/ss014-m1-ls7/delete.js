146b80ff40071794153c7ed564389042ee12aa29
LianPro <boyintrouble.1310@gmail.com>
2026-05-11 17:28:52 +0700

    ss014-m1-ls7

---
+++ ss014-m1/ss014-m1-ls7/delete.js
@@ -0,0 +1,23 @@
+function deleteValue() {
+
+    let numbers = [5, 12, 8, 20, 7, 15, 3, 10, 25, 1];
+
+    let V = parseInt(document.getElementById("value").value);
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (numbers[i] === V) {
+
+            for (let j = i; j < numbers.length - 1; j++) {
+                numbers[j] = numbers[j + 1];
+            }
+
+            numbers[numbers.length - 1] = 0;
+
+            break;
+        }
+    }
+
+    document.getElementById("result").innerText =
+        numbers.join(", ");
+}
