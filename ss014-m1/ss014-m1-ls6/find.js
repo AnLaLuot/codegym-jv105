c39bf02a0f3fb29da97a12ee41e47c7167583939
LianPro <boyintrouble.1310@gmail.com>
2026-05-11 17:26:46 +0700

    ss014-m1-ls6

---
+++ ss014-m1/ss014-m1-ls6/find.js
@@ -0,0 +1,24 @@
+function findValue() {
+
+    let numbers = [5, 12, 8, 20, 7, 15, 3, 10, 25, 1];
+
+    let V = parseInt(document.getElementById("value").value);
+
+    let found = false;
+
+    for (let i = 0; i < numbers.length; i++) {
+
+        if (numbers[i] === V) {
+            found = true;
+            break;
+        }
+    }
+
+    if (found) {
+        document.getElementById("result").innerText =
+            V + " is in the array";
+    } else {
+        document.getElementById("result").innerText =
+            V + " is not in the array";
+    }
+}
