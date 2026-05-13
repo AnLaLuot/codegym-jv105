bcbc7ba9fd49f85cdaac4d1504c9ecc423fcac9c
LianPro <boyintrouble.1310@gmail.com>
2026-05-13 20:35:37 +0700

    fix ss014-m1-ls4

--- ss014-m1/ss014-m1-ls4/reverse.js
+++ ss014-m1/ss014-m1-ls4/reverse.js
@@ -1,7 +1,15 @@
 function reverseArray() {
+
     let numbers = [1, 2, 3, 4, 5];
 
-    numbers.reverse();
+    for (let i = 0; i < numbers.length / 2; i++) {
+
+        let temp = numbers[i];
+
+        numbers[i] = numbers[numbers.length - 1 - i];
+
+        numbers[numbers.length - 1 - i] = temp;
+    }
 
     document.getElementById("result").innerText =
         numbers.join(", ");
