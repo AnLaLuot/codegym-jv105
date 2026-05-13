676cea9064b17a4114cca175e6e09f54565152a0
LianPro <boyintrouble.1310@gmail.com>
2026-05-13 18:19:39 +0700

    ss014-m1-btm

--- ss014-m1/ss014-m1-btm3/convert.js
+++ ss014-m1/ss014-m1-btm3/convert.js
@@ -1,16 +1,16 @@
-function convert() {
+function convert(): void {
+
     let input = document.getElementById("text").value;
 
     let result = "";
 
     for (let i = 0; i < input.length; i++) {
+
         let char = input[i];
 
-        if (char === char.toUpperCase()) {
-            result += char.toLowerCase();
-        } else {
-            result += char.toUpperCase();
-        }
+        result += char === char.toUpperCase()
+            ? char.toLowerCase()
+            : char.toUpperCase();
     }
 
     document.getElementById("result").innerText = result;
