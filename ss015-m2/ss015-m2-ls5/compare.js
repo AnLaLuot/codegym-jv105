7abefe3a08a6bd8d0d19ab05cda813a3f8ee2453
LianPro <boyintrouble.1310@gmail.com>
2026-05-22 19:13:34 +0700

    ss015-m2-ls5-fix

--- ss015-m2/ss015-m2-ls5/compare.js
+++ ss015-m2/ss015-m2-ls5/compare.js
@@ -1,13 +1,10 @@
 function compareStrings() {
 
     let str1 = document.getElementById("str1").value;
+
     let str2 = document.getElementById("str2").value;
 
-    if (str1 === str2) {
-        document.getElementById("result").innerText =
-            "Hai chuỗi giống nhau";
-    } else {
-        document.getElementById("result").innerText =
-            "Hai chuỗi khác nhau";
-    }
+    document.getElementById("result").innerText = (str1 === str2)
+            ? "Hai chuỗi giống nhau"
+            : "Hai chuỗi khác nhau";
 }
