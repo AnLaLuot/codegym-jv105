12ed83672e0d9014f33540bcaeeec828a772ff02
LianPro <boyintrouble.1310@gmail.com>
2026-05-22 20:13:47 +0700

    ss012-vl1-ls6-fix

--- ss012-vl1/ss012-vl1-ls6/sum7.js
+++ ss012-vl1/ss012-vl1-ls6/sum7.js
@@ -1,10 +1,15 @@
 function calculate() {
+
+    const TOTAL_NUMBERS = 30;
+
+    const DIVISIBLE_NUMBER = 7;
+
     let sum = 0;
 
-    for (let i = 1; i <= 30; i++) {
-        sum += 7 * i;
+    for (let count = 1; count <= TOTAL_NUMBERS; count++) {
+        sum += count * DIVISIBLE_NUMBER;
     }
 
     document.getElementById("result").innerText =
-        "Tổng là: " + sum;
+        `Tổng là: ${sum}`;
 }
